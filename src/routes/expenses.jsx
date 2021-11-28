11ec82cb37486a5c4ed0f1689ec612ae45b58fd7
Artem-SP <artem567gp@gmail.com>
2021-11-29 00:01:44 +0200

    first commit

---
+++ src/routes/expenses.jsx
@@ -0,0 +1,8 @@
+export default function Expenses() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Expenses</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et voluptatem ut consectetur aspernatur officia dolor ea quidem cumque dolorum, ducimus earum porro voluptatibus inventore aliquam illo explicabo nostrum id fuga?</p>
+    </main>
+  );
+}
